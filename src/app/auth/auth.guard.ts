7bbc5b245cb7f284c9b00f006df54c638b3a85e2
lujia <lujia4723@gmail.com>
2023-07-16 18:39:20 +0200

    added a routerguard into recipe page. sodass cant access the recipes page without authenticate

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,6 +1,47 @@
-import {CanActivate} from "@angular/router";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
 import {Injectable} from "@angular/core";
-@Injectable()
+import {map, Observable} from "rxjs";
+import {AuthService} from "./auth.service";
+import {take} from "rxjs/operators";
+@Injectable({providedIn : 'root'})
 export class AuthGuard implements CanActivate{
+
+    constructor(private as : AuthService,
+                private router : Router) {
+    }
+    canActivate(
+        route: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot):
+        Observable<boolean | UrlTree > | Promise<boolean | UrlTree> | boolean | UrlTree{
+
+        return this.as.user.pipe(
+            take(1),
+            //只订阅一次
+            map(
+            user =>{
+                const isAuth = !!user;
+                // 是否验证 和是否存在 已验证的user相关
+                if(isAuth){
+                    return isAuth;
+                    //已验证， 返回true
+                }
+                return this.router.createUrlTree(['/auth'])
+                //否则重定向到 登录页面
+            }
+        ) )
+    }
+
+
+    // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    //     let isAuthenticated = false;
+    //
+    //     this.as.user.subscribe(user => {
+    //         isAuthenticated = !!user;
+    //     });
+    //
+    //     return isAuthenticated;
+    // }
+
+
 
 }
